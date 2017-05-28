fc1a173f49703da5fc384324c8f170ffcaa2a465
TehScratchGuy <TehScratchGuy@gmail.com>
2017-05-27 20:23:09 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
 
-echo '<html><head><title>Chat</title></head><body><p>Make sure to scroll all the way down to see newer messages!</p><p><a href="dashboard.php">Refresh</a></p><p><iframe width="838" height="422" src="messages.php"></iframe></p><p><form action="send.php" method="POST"><p>Nickname:</p><input type="text" name="name"><p>Message:</p><input type="text" name="message"><input type="submit" value="Send">';
+echo '<html><head><title>Chat</title></head><body><p>Make sure to scroll all the way down to see newer messages!</p><p><a href="dashboard.php">Refresh</a></p><p><iframe width="838" height="422" src="messages.txt"></iframe></p><p><form action="send.php" method="POST"><p>Nickname:</p><input type="text" name="name"><p>Message:</p><input type="text" name="message"><input type="submit" value="Send">';
 
 ?>
